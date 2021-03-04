096cc3332b87481f808ef685d6e7be9cc54592d5
kurwitz3 <kurwitz3@aol.com>
2021-03-04 17:19:12 -0500

    commit tree 5538cf6d52d11b05e6761d143ae220f99446768d

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(e) => e.target = `I'm sad now`} />
   </div>,
   document.getElementById('root')
 );
